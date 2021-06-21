1b6613dfb0416ab7ada2507f36dba46e624a811a
YogeshYadav-devit <86092759+YogeshYadav-devit@users.noreply.github.com>
2021-06-21 19:24:41 +0530

    Add files via upload
    Learn the Typescript concept and Practice these Concept

---
+++ interface.js
@@ -0,0 +1,52 @@
+var array1;
+array1 = ['adda', 'dafadf', 'dadfd'];
+array1[1] = 'af';
+for (var i = 0; i < array1.length; i++) {
+    console.log(array1[i]);
+}
+// readonly array
+var readOnlyArrY = array1;
+// readOnlyArrY[1]="sadfaf";
+// readOnlyArrY.push("adf");
+for (var i_1 = 0; i_1 < readOnlyArrY.length; i_1++) {
+    console.log(readOnlyArrY[i_1]);
+}
+// comvert readonly array to normal array
+array1 = readOnlyArrY;
+var Three = /** @class */ (function () {
+    function Three() {
+        this.p = "Hello";
+    }
+    return Three;
+}());
+var x = { p: "hi" };
+var two = x;
+two = new Three();
+// union 
+function start(// arg: string | string[] | (() => string) | { s: string } is a type of variable
+arg) {
+    // this is super common in JavaScript
+    if (typeof arg === "string") {
+        return commonCase(arg);
+    }
+    else if (Array.isArray(arg)) {
+        return arg.map(commonCase).join(",");
+    }
+    else if (typeof arg === "function") {
+        return commonCase(arg());
+    }
+    else {
+        return commonCase(arg.s);
+    }
+    function commonCase(s) {
+        // finally, just convert a string to another string
+        return s;
+    }
+}
+var employee1 = {};
+employee.name = "John"; // OK
+employee.code = 123; // OK
+console.log(employee1.name);
+var kv1 = { key: 1, value: "Steve" }; // OK
+var kv2 = { key: 1, val: "Steve" }; // Compiler Error: 'val' doesn't exist in type 'KeyPair'
+var kv3 = { key: 1, value: 100 }; // Compiler Error: 
---
+++ union.ts
@@ -0,0 +1,24 @@
+export
+let CarName: string|number;
+CarName= "Audi"
+console.log(typeof(CarName));
+CarName=1232;
+console.log(typeof(CarName));
+let arrayName: string[]|number[];
+arrayName=[1.2,3,23545];
+console.log(typeof(arrayName));
+arrayName=["Yogesh","rao"];
+console.log(typeof(arrayName));
+
+// any type 
+let something: any = "Hello World!"; 
+console.log(typeof(something));
+something = 23;
+console.log(typeof(something));
+something = true;
+console.log(typeof(something));
+// type of any array
+let arr: any[] = ["John", 212, true]; 
+arr.push("Smith"); 
+arr.push("Sith"); 
+console.log(arr); //Output: [ 'John', 212, true, 'Smith' ] 
